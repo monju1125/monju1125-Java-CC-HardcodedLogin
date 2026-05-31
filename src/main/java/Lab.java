0d77cad8795e3f9658d873e738489cc212331642
monju1125 <monju1125@github.com>
2026-05-31 03:20:33 +0000

    Test run: 5/7 passed, 2 failed, 0 skipped - 2026-05-31T03:20:33.723Z

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -15,6 +15,8 @@ public class Lab {
      * @return true if there is a successful login, and false otherwise.
      */
     public boolean login(String username, String password){
+        if (this.equals(username) && this.equals(password)) return true;
+        else
         return false;
     }
 }
